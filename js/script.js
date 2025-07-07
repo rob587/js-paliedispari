4a6d57be3de243157d2899b06523ff03f78670cd
rob587 <robertocamm02@yahoo.com>
2025-07-07 16:09:49 +0200

    dichiarazione Varabiale

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Palidroma
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+
+//dichiarazione variabile//
+const nameRequest = prompt("Inserisci il nome")
